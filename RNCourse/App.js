5e3b7f93eb0a000a7dd66875707d05c2a1810cdd
dongyoon7212 <dongyoon7212@naver.com>
2023-05-01 14:43:06 +0900

    레이아웃 개선하기

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -7,7 +7,7 @@ export default function App() {
         <TextInput placeholder="Your course goal!" style={styles.textInput} />
         <Button title="Add Goal" />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
       </View>
     </View>
@@ -17,17 +17,27 @@ export default function App() {
 // 스타일시트를 사용했을때 자동완성 기능을 유용하게 사용할 수 있으며 코드 검증 기능이 내장되어 있다.
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
   },
   textInput: {
     borderWidth: 1,
     borderColor: "#cccccc",
-    width: "80%",
+    width: "70%",
     marginRight: 8,
     padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
   },
 });
